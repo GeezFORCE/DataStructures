eecd26dcbf1b2863a6e0a5b471383b637f6a214d
C G Giridhar <girigov39@gmail.com>
2020-05-17 09:55:01 +0530

    Insertion Sort

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -2,46 +2,47 @@
 using namespace std;
 int main()
 {
-    void selection_sort(vector<int>);
-    void display_vector(vector<int>);
+    void selection_sort(vector<int> *);
+    void display_vector(vector<int> *);
     int size, inp;
     cout << "Enter the size: ";
     cin >> size;
     //--size;
     cout << endl;
-    vector<int> arr;
+    vector<int> *arr = new vector<int>;
     cout << "Enter the inputs : ";
     while (size--)
     { //&& size>=0){
         cin >> inp;
-        arr.push_back(inp);
+        (*arr).push_back(inp);
     }
     selection_sort(arr);
     return 0;
 }
-void selection_sort(vector<int> arr)
+void selection_sort(vector<int> *arr)
 {
-    void display_vector(vector<int>);
+    void display_vector(vector<int> *);
     int key, j;
-    for (int i = 1; i < arr.size(); i++)
+    for (int i = 1; i < (*arr).size(); i++)
     {
-        key = arr.at(i);
+        key = (*arr).at(i);
         j = i - 1;
-        while (j >= 0 && arr.at(j) > key)
+        while (j >= 0 && (*arr).at(j) > key)
         {
-            arr.at(j + 1) = arr.at(j);
+            (*arr).at(j + 1) = (*arr).at(j);
             j--;
         }
-        arr.at(j + 1) = key;
-        cout << "At stage " << i + 1 << ":\t";
+        (*arr).at(j + 1) = key;
+        cout << "At stage " << i << ":\t";
         display_vector(arr);
     }
-    cout << "After Sorting \n";
+    cout << endl;
+    cout << "After Sorting : ";
     display_vector(arr);
 }
-void display_vector(vector<int> arr)
+void display_vector(vector<int> *arr)
 {
-    for (auto &i : arr)
+    for (auto &i : (*arr))
         cout << i << "\t";
     cout << endl;
 }
