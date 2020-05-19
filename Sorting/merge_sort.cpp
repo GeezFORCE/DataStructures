ab39287cdda5182606fb0966a6eaf2dc187e26be
C G Giridhar <girigov39@gmail.com>
2020-05-19 15:40:04 +0530

    Merge Sort Improved

--- Sorting/merge_sort.cpp
+++ Sorting/merge_sort.cpp
@@ -1,63 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int merge(int arr[100], int first, int mid, int last)
+void merge(int arr[], int first, int mid, int last)
 {
     int lpt = first, upt = mid + 1, size = last - first + 1, j = 0, b[size];
     while (lpt < mid + 1 && upt < last + 1)
     {
         if (arr[lpt] < arr[upt])
-        {
-            b[j] = arr[lpt];
-            lpt++;
-            j++;
-        }
+            b[j++] = arr[lpt++];
         else
-        {
-            b[j] = arr[upt];
-            upt++;
-            j++;
-        }
+            b[j++] = arr[upt++];
     }
     while (lpt < mid + 1)
-    {
-        b[j] = arr[lpt];
-        lpt++;
-        j++;
-    }
+        b[j++] = arr[lpt++];
     while (upt < last + 1)
-    {
-        b[j] = arr[upt];
-        upt++;
-        j++;
-    }
+        b[j++] = arr[upt++];
     for (lpt = first, j = 0; j < size; lpt++, j++)
         arr[lpt] = b[j];
 
-    return 1;
+    return;
 }
 
-int merge_sort(int arr[100], int first, int last)
+void merge_sort(int arr[], int first, int last)
 {
     int mid;
     if (first == last)
-        return 1;
+        return;
     else
     {
         mid = (first + last) / 2;
         merge_sort(arr, first, mid);
         merge_sort(arr, mid + 1, last);
         merge(arr, first, mid, last);
-        return 1;
     }
 }
 
 int main()
 {
-    int arr[100], size;
+    int size;
     cout << "Enter array size : ";
     cin >> size;
     cout << endl;
+    int arr[size];
     cout << "Enter the elements : ";
     for (int i = 0; i < size; i++)
         cin >> arr[i];
@@ -68,24 +52,3 @@ int main()
         cout << arr[i] << "\t";
     return 0;
 }
-
-/*void merge(int arr[100],int first,int mid,int last){
-    int n1=mid-first+1,n2=last-mid,size=last-first+1;
-    int left[n1],right[n2],i;
-    for(i=first;i<n1;i++)
-        left[i]=arr[i];
-    for(i=mid;i<n2;i++)
-        left[i]=arr[i];
-    i=first;
-    while(i<size){
-        if(left[i]<right[i]){
-            arr[i]=left[i];
-            i++;
-        }
-        else{
-            arr[i]=right[i];
-            i++;
-        }
-    }
-}
-*/
