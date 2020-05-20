0337e4b7cd33dc212309931633c3bf8b9c9e6f5b
C G Giridhar <girigov39@gmail.com>
2020-05-20 11:43:55 +0530

    Count Sort Formatted

--- Sorting/count_sort.cpp
+++ Sorting/count_sort.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void CountSort(int A[], int size){
-    int *max = max_element(A,A+size);
-    int max_size = (*max)+1;
-    int C[(*max)+1];
+void CountSort(int A[], int size)
+{
+    int max_size = (*max_element(A, A + size)) + 1;
+    int C[max_size];
     for (int i = 0; i < max_size; i++)
-        C[i]=0;
+        C[i] = 0;
     for (int i = 0; i < size; i++)
         C[A[i]]++;
-    for (int i = 0; i < size; i++)
-        cout<<C[i]<<" ";
-    cout<<endl;
-    int i=0,j=0;
-    while(i<max_size){
-        if(C[i]>0){
-            A[j++]=i;
+    cout << endl;
+    int i = 0, j = 0;
+    while (i < max_size)
+    {
+        if (C[i] > 0)
+        {
+            A[j++] = i;
             C[i]--;
         }
         else
